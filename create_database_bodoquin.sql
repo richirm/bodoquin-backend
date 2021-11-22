690ba6d653cfbf338a33ef3466af1bfce8e44f88
richirm <richirm89@hotmail.com>
2021-11-21 23:07:33 -0500

    feat(create_database_bodoquin.sql): crear tabla de modulos

--- create_database_bodoquin.sql
+++ create_database_bodoquin.sql
@@ -6,7 +6,7 @@ ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'bodoquin
 flush privileges;
 /************************************/
 
-/********* CREA TABLAS **************/
+/******** CREA TABLA REPOSTERIA ******/
 CREATE TABLE Productos_Reposteria (
   idProducto int NOT NULL AUTO_INCREMENT,
   nombreImg varchar(255),
@@ -20,8 +20,32 @@ CREATE TABLE Productos_Reposteria (
 );
 /*************************************/
 
-/********** INSERTA DATA ************/
+/******** CREA TABLA JUGUETERIA ******/
+
+/*************************************/
+
+/******** CREA TABLA TECNOLOGIA ******/
+
+/*************************************/
+
+/********** CREA TABLA MODA **********/
+
+/*************************************/
+
+/****** INSERTA DATA REPOSTERIA ******/
 INSERT INTO Productos_Reposteria (idProducto, nombreImg, nombreCategoria, nombreProducto, descripcionProducto, precioProducto, cantidadProducto, especificaciones) VALUES (null, "torta_chocolate_fresa.jpg", "Torta húmeda", "Torta de leche de Óreo", "Torta rica en su sabor, ha ganado el premio internacional, hecha con agua manantial", 60.25, 0, "Fecha de vencimiento: 01/11/2021|Elaborado por: Panadería Bodoquin|Ingredientes: Galletas óreo, Leche, Huevos, etc.");
 INSERT INTO Productos_Reposteria (idProducto, nombreImg, nombreCategoria, nombreProducto, descripcionProducto, precioProducto, cantidadProducto, especificaciones) VALUES (null, "torta_selva_negra.jpg", "Torta húmeda", "Selva negra de cerezas", "Compuesta por capas de bizcochuelo de chocolate embebido en kirsch e intercaladas con nata y cerezas.", 55.50, 0, "Fecha de vencimiento: 01/11/2021|Elaborado por: Panadería Bodoquin|Ingredientes: Galletas óreo, Leche, Huevos, etc.");
+
+/*************************************/
+
+/****** INSERTA DATA JUGUETERIA ******/
+
+/*************************************/
+
+/****** INSERTA DATA TECNOLOGIA ******/
+
+/*************************************/
+
+/******** INSERTA DATA MODA **********/
 
 /*************************************/
