23d2ceb5a54b0a9e49f797ddc9dd911af7249051
wolfdark87 <david.roma.878@gmail.com>
2021-12-04 00:39:39 -0500

    feat(app) : se agrega endpoints comidas

--- src/app.js
+++ src/app.js
@@ -46,6 +46,27 @@ app.get('/cursos', function (req, res) {
   ];
   res.send(cursos);
 });
+
+app.get('/comidas', function (req, res) {
+  const comidas = [
+    {
+      nombre: 'Cebiche',
+      precio: 29.90,
+      ingredientes: 'pescado'
+    },
+    {
+      nombre: 'Chaufa',
+      precio: 15.00,
+      ingredientes: 'pollo'
+    },
+    {
+      nombre: 'Carapúlcra',
+      precio: 18.00,
+      ingredientes: 'papa seca'
+    }
+  ];
+  res.send(comidas);
+});
  
 app.listen(3000, function () {
   console.log('La aplicación se ha levantado en el puerto 3000');
