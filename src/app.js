5e01a1d59881e3d65660af27e3e4c1133ef028a7
Vanessa Chipana <vanessa.cintya.chipana.isidro@everis.nttdata.com>
2021-12-19 01:06:30 -0500

    feat(app): creacion de enpoint get de jugueteria

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ app.use('/cultura', require('./routes/cultura/centros-turisticos'));
 app.use('/animales', require('./routes/animales/mascotas'));
 
 app.use('/productos', require('./routes/productos/reposteria'));
+app.use('/productos', require('./routes/productos/jugueteria'));
 /**************************/
 
 /******* Arranque ********/
