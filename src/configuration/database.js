174645b68ee73d6d49b05fff5b850bb83f970db5
richirm <richirm89@hotmail.com>
2021-12-30 00:04:33 -0500

    feat(database): usa bodoquin_bd

--- src/configuration/database.js
+++ src/configuration/database.js
@@ -5,7 +5,7 @@ module.exports = {
     var connection = mysql.createConnection({
       host: 'localhost',
       port: 3306,
-      database: 'bodoquin_bd_demo',
+      database: 'bodoquin_bd',
       user: 'root',
       password: 'bodoquin123',
     });
