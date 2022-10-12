18011996430de2f14ba2eaf7d7acb0559f409bba
wolfdark87 <david.roma.878@gmail.com>
2022-10-12 00:01:43 -0500

    feat(videojuegos) creacion de endpoint de producto/id .

--- src/routes/productos/videojuegos.js
+++ src/routes/productos/videojuegos.js
@@ -22,6 +22,27 @@ router.get('/videojuegos', function(req, res) {
   );
 });
 
+router.get('/videojuegos/:idProducto', function(req, res) {
+  database.conectar(
+    (connection) => {
+      connection.query(
+        `SELECT * FROM Productos_Videojuegos
+         WHERE idProducto = ${req.params.idProducto}`,
+        (error, result) => {
+          if(!!error) {
+            res.send(error);
+          } else {
+            res.send(result[0]);
+          }
+        }
+      );
+    },
+    (error) => {
+      res.send(error);
+    }
+  );
+});
+
 router.get('/videojuegos/servicios/soporte/controles', function(req, res) {
   //Pendiente la conexion a la BD
   const controles = [
